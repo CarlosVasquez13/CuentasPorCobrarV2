721b9cb31fea895db76297738fa206ceca23dd3d
Davidcpu <daviddurandz67@gmail.com>
2021-08-09 05:05:17 -0600

    Update RegistroCliente.aspx.cs

--- CuentasPorCobrar/RegistroCliente.aspx.cs
+++ CuentasPorCobrar/RegistroCliente.aspx.cs
@@ -9,9 +9,26 @@ namespace CuentasPorCobrar
 {
     public partial class RegistroCliente : System.Web.UI.Page
     {
+        MySql.Data.MySqlClient.MySqlConnection conn;
+        MySql.Data.MySqlClient.MySqlCommand cmd;
+        string query = "";
+        MySql.Data.MySqlClient.MySqlDataReader reader;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+        protected void btn_registroUsuario(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_AgregarTelReferencia(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
