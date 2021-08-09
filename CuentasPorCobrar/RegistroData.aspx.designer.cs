947646556637525d80f46d61eb97364507fe65db
Davidcpu <daviddurandz67@gmail.com>
2021-08-09 03:41:57 -0600

    Update RegistroData.aspx.designer.cs

--- CuentasPorCobrar/RegistroData.aspx.designer.cs
+++ CuentasPorCobrar/RegistroData.aspx.designer.cs
@@ -13,5 +13,77 @@ namespace CuentasPorCobrar
 
     public partial class RegistroData
     {
+
+        /// <summary>
+        /// Control IdEstadoCuentanum.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl IdEstadoCuentanum;
+
+        /// <summary>
+        /// Control DescripcionText.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText DescripcionText;
+
+        /// <summary>
+        /// Control IdPaisnum.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl IdPaisnum;
+
+        /// <summary>
+        /// Control PaisText.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText PaisText;
+
+        /// <summary>
+        /// Control IdDepanum.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl IdDepanum;
+
+        /// <summary>
+        /// Control DepartamentoText.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText DepartamentoText;
+
+        /// <summary>
+        /// Control IdMunisnum.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl IdMunisnum;
+
+        /// <summary>
+        /// Control nombremuniText.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText nombremuniText;
     }
 }
