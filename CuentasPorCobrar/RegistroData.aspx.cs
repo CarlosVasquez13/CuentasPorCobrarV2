33eae1dbff23b038a70b4b9fe0bc9b0f8c40fd26
Davidcpu <daviddurandz67@gmail.com>
2021-08-14 02:08:46 -0600

    Update RegistroData.aspx.cs

--- CuentasPorCobrar/RegistroData.aspx.cs
+++ CuentasPorCobrar/RegistroData.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace CuentasPorCobrar
 {
@@ -12,6 +14,38 @@ namespace CuentasPorCobrar
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            if (!IsPostBack)
+            {
+                DropListPais();
+            }
+
         }
+
+        private void DropListPais()
+        {
+
+            string mysqlConnString = "Data Source=Localhost;Database=example;User Id=root; password='Graduacion12'";
+
+            using (MySqlConnection cn= new MySqlConnection(mysqlConnString))
+            {
+                MySqlDataAdapter adp = new MySqlDataAdapter("SELECT pais.Nombre  " +
+                    "FROM departamento, pais WHERE departamento.Id_Pais = pais.Id_Pais;", cn);
+                DataTable dt = new DataTable();
+                int v = adp.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    ListaPais.DataSource = dt;
+                    ListaPais.DataTextField = "Pais";
+                    ListaPais.DataValueField = "ID";
+                    ListaPais.DataBind();
+
+                }
+            }
+
+        }
+
+       
+
+
     }
 }
